e027d491ccbf06f1c7a6929c45a4c83ebb44f42f
Eric Dew <me@ericdew.com>
2017-06-21 00:38:02 -0500

    Update service worker

--- sw.js
+++ sw.js
@@ -13,29 +13,33 @@ var CACHE_NAME = 'cast-cache-v1';
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
-  
+
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
     return cache.addAll(urlsToCache);
   }));
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
+  console.log('updating cache');
 
-    fetch(event.request)
-    .then(function(event){
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function (response) {
-          return response || fetch(event.request).then(function(response) {
-            cache.put(event.request, response.clone());
-            return response;
-          });
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
         });
-      })
+      });
     })
-    .catch(function() {
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetching network first');
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
       return caches.match(event.request);
     })
-
   );
 });
