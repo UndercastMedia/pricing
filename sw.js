97f7defa58fd7285ef443a1f30e460a2b64e83ff
Eric Dew <me@ericdew.com>
2017-06-20 23:41:33 -0500

    Test different offline strategy

--- sw.js
+++ sw.js
@@ -11,14 +11,23 @@ urlsToCache.push("{{ page.url }}");
 
 var CACHE_NAME = 'cast-cache-v1';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-    return cache.addAll(urlsToCache);
-  }).catch(function(err) {
-    console.log('cache add err', err);
-  }));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
