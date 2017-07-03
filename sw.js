d0bb98ec1ae87045355262806341ce744ac5253d
Eric Dew <me@ericdew.com>
2017-07-03 01:13:12 -0500

    Update Service Worker Strategy
    All requests will be fetched from the network. If the network fails or takes longer than 3 seconds to load the resource, the request will be served from the cache instead.

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ urlsToCache.push("{{ page.url }}");
 var CACHE = 'cast-cache-v1';
 
 self.addEventListener('install', function(evt) {
+  self.skipWaiting();
   evt.waitUntil(precache());
 });
 
@@ -22,9 +23,11 @@ function precache() {
 }
 
 self.addEventListener('fetch', function(evt) {
-  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+  evt.respondWith(fromNetwork(evt.request, 3000).catch(function () {
     return fromCache(evt.request);
   }));
+  
+  evt.waitUntil(updateCache(evt.request));
 });
 
 function fromNetwork(request, timeout) {
@@ -41,6 +44,14 @@ function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function updateCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
     });
   });
 }
